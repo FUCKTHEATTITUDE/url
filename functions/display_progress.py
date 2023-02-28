468c7ae169cbe74c36deae6ea8b66a93f22e58e7
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-02-28 21:05:14 +0530

    Update display_progress.py

--- functions/display_progress.py
+++ functions/display_progress.py
@@ -54,7 +54,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
 def humanbytes(size):
     if not size:
         return ""
-    power = 2 ** 10
+    power = 4 ** 10
     n = 0
     Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
