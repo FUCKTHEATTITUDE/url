d477180d95af786351fd4b42fb81d8db81b127e7
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-03-01 08:27:45 +0530

    Update translation.py

--- plugins/translation.py
+++ plugins/translation.py
@@ -30,7 +30,7 @@ I Am Telegram URL Uploader Bot.
 ➪ Send /showthumb To View Custom Thumbnail 
  
 """
-    ABOUT_TEXT = await message.reply_photo(photo=THUMBNAIL,caption="""**PAY AND SEND SCREEN SHOT OF THE PAYMENT TO GET PREMIUM PAKAGE UPLOAD ALL URL,UNLIMITED UPLOAD PER MONTH 65 RUPES INR to @alpha_romeo_06**")
+ await message.reply_photo(photo=THUMBNAIL,ABOUT_TEXT ="""**PAY AND SEND SCREEN SHOT OF THE PAYMENT TO GET PREMIUM PAKAGE UPLOAD ALL URL,UNLIMITED UPLOAD PER MONTH 65 RUPES INR to @alpha_romeo_06**")
 
 """
     PROGRESS = """
